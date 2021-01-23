7b26b098fdfade3dded64e41d685048949402eb7
CecaCora <52695450+CecaCora@users.noreply.github.com>
2021-01-24 04:34:25 +0800

    Navigation now displays all links by default

--- src/Nav.js
+++ src/Nav.js
@@ -2,12 +2,15 @@ const Nav = (() => {
   console.log('Nav component successfully activated!');
 
   let accordions = document.querySelectorAll('.acc-header');
+  let panels = document.querySelectorAll('.acc-panel');
+
+  panels.forEach((panel) => {
+    panel.style.maxHeight = `${panel.scrollHeight}px`;
+  });
 
   accordions.forEach((accordion) => {
     accordion.addEventListener('click', (e) => {
       let panel = e.target.nextElementSibling;
-
-      console.log(panel);
 
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
