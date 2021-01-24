5053341b3aae888284babe7ba9bc7d18ef8c60c9
CecaCora <52695450+CecaCora@users.noreply.github.com>
2021-01-24 20:56:07 +0800

    Nav has carets now!

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,23 @@
+import Caret from './icons/Caret';
+
 const Nav = (() => {
   console.log('Nav component successfully activated!');
 
   let accordions = document.querySelectorAll('.acc-header');
   let panels = document.querySelectorAll('.acc-panel');
 
+  // for (let i = 0; i < Array.from(accordions).length; i++) {
+  //   Array.from(accordions)[i].appendChild(caret);
+  // }
+
   panels.forEach((panel) => {
     panel.style.maxHeight = `${panel.scrollHeight}px`;
   });
 
   accordions.forEach((accordion) => {
+    let caret = Caret();
+    accordion.appendChild(caret);
+
     accordion.addEventListener('click', (e) => {
       let panel = e.target.nextElementSibling;
 
@@ -16,6 +25,15 @@ const Nav = (() => {
         panel.style.maxHeight = null;
       } else {
         panel.style.maxHeight = `${panel.scrollHeight}px`;
+      }
+
+      let caret = e.target.firstElementChild;
+      console.log(caret);
+
+      if (caret.style.transform) {
+        caret.style.transform = null;
+      } else {
+        caret.style.transform = 'rotate(-90deg)';
       }
     });
   });
